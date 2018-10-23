78dc041fe408df20622e7a0a9b090455cfeeddcc
Jerome Shapiro <jerome@dimebox.com>
2018-10-23 10:04:53 +0200

    Using Cmake

---
+++ src/thread_pool.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Jerome Shapiro on 2018-10-19.
+//
+
+#include <memory>
+#include <future>
+#include <functional>
+
+#include <thread_pool.h>
+#include <runner.h>
+
+int ThreadPool::delegateRunner() {
+    if (i == thread_count) {
+        i = 0;
+    }
+
+    return i++;
+}
+
+ThreadPool::ThreadPool(size_t c) : thread_count(c), thread_pool(c) {
+    for (auto i = 0; i < thread_count; i++) {
+        thread_pool[i] = std::make_unique<Runner>(i);
+    }
+}
+
+template<typename Func, typename ... Arg>
+auto ThreadPool::assign(Func&&, Arg&& ... args) -> std::future<decltype(func(args...))> {
+    std::function<decltype(func(args...))()> callable = std::bind(std::forward<Func>(func),
+            std::forward<Arg>(args)...);
+
+    auto task = std::make_shared<std::packaged_task<decltype(func(args...))()>>(callable);
+
+    auto delegatedRunner = delegateRunner();
+
+    thread_pool[delegatedRunner]->assign([task]() {
+        (*task)();
+    });
+
+    return task->get_future();
+}
